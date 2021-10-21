83fe1bc56d25375656f1e89cec5034073183d45d
anastorr <nastya.stor111@gmil.com>
2021-10-22 01:48:25 +0300

    EM algorithm prototype

--- main.py
+++ main.py
@@ -3,11 +3,37 @@ from keras.datasets import mnist
 from matplotlib import pyplot
 
 
-def em(train_set, classes):
+def prob_update(theta0, theta1, x, p):
+    product = np.prod(np.power(np.divide(theta1, theta0), x)*np.power(np.divide(1-theta1, 1-theta0), 1-x), axis=(1,2))
+    return  1/(1+(1-p)/p*product)
+
+
+def em(x, classes):
     # initialising p(k|x) with random values
-    n = train_set.shape[0]
-    p0 = np.random.uniform(0, 1, n)
-    p1 = 1 - p0
+    x = np.squeeze(x)
+    n = x.shape[0]
+    p0_x = np.random.uniform(0, 1, n)
+    p1_x = 1 - p0_x
+
+    delta = 1
+    theta0 = np.zeros(x.shape[1:])
+    theta1 = np.zeros(x.shape[1:])
+
+    while delta > 0.001:
+        print('hop')
+        theta0_new = np.sum(x * p0_x[:, np.newaxis, np.newaxis], axis=0) / np.sum(p0_x)
+        theta1_new = np.sum(x * p1_x[:, np.newaxis, np.newaxis], axis=0) / np.sum(p1_x)
+
+        p0 = np.sum(p0_x)/n
+
+        p0_x = prob_update(theta0_new, theta1_new, x, p0)
+        p1_x = 1 - p0_x
+
+        delta = abs(theta0_new - theta0).max()
+        theta0 = theta0_new
+        theta1 = theta1_new
+
+    return (theta0, theta1)
 
 
 if __name__ == '__main__':
@@ -16,5 +42,5 @@ if __name__ == '__main__':
     indices = np.argwhere((train_y == 0) | (train_y == 1))
     train_set = np.where(train_X[indices] > 0, 1, 0)
 
-    em(train_set, [0, 1])
+    theta0, theta1 = em(train_set, [0, 1])
     print('hi')
