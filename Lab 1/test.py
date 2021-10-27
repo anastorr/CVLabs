62141bdd8246636695680a173377275317e040f1
anastorr <nastya.stor111@gmil.com>
2021-10-27 20:40:31 +0300

    Separated tests

--- Lab 1/test.py
+++ Lab 1/test.py
@@ -2,8 +2,14 @@ from main import em
 import numpy as np
 
 
-def test_em():
+def test_em_0():
     assert em(np.array([[[1]], [[1]]])) == (np.array([[1]]), np.array([[1]]))
+
+
+def test_em_1():
     assert em(np.array([[[0]], [[0]]])) == (np.array([[0]]), np.array([[0]]))
+
+
+def test_em_2():
     theta0, theta1 = em(np.array([[[0.5]], [[0.1]]]))
     assert 0 <= theta0 <= 1 and 0 <= theta1 <= 1
