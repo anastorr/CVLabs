5268787aa1432b9f35107203848d32b614c2c8ad
anastorr <nastya.stor111@gmail.com>
2021-12-09 22:27:52 +0200

    Arc consistency added

--- Lab3/functions.py
+++ Lab3/functions.py
@@ -3,40 +3,44 @@ import json
 
 
 def load(file):
-    sudoku = np.array(json.load(file)).reshape(81)
+    sudoku = np.array(json.load(file))
     return sudoku
-    # # rearranging sudoku into arrays of neighbours for each element
-    # neighbours = np.zeros(81, 20)
-    # # i don't know how to do this without loops :'(
-    # for i in range(81):
-    #     # neighbours[i, :(i-1)%9] = sudoku[(i-1)//9, :(i-1)%9]
-    #     # neighbours[i, (i-1)%9:8] = sudoku[(i-1)//9, (i-1)%9+1:]
-    #     # neighbours[i, 8:8+(i-1)//9] = sudoku[:(i-1)//9, (i-1)%9]
-    #     # neighbours[i, 8+(i-1)//9:16] = sudoku[(i-1)//9:, (i-1)%9]
-    #     sudoku_unfold = sudoku.reshape(1, 81)
-    #     row = i//9
-    #     col = i%9
-    #     neighbours[i, :8] =
 
 
-def neighbours_idx(idx):
-    neighb_idx = np.arange(0, 20, 1)
+def neighbours_idx(idx, sudoku):
+    row = idx // 9
+    col = idx % 9
+    section_row = row // 3
+    section_col = col // 3
+    indices = np.arange(0, 81, 1).reshape(9, 9)
+    section = np.copy(indices[3*section_row:3*section_row+3, 3*section_col:3*section_col+3])
+    section[row % 3] = -1
+    section[:, col % 3] = -1
+    neighb_idx = np.concatenate((indices[row, :col], indices[row, col+1:], indices[:row, col], indices[row+1:, col],
+                                 section[section != -1]))
     return neighb_idx
 
 
 def solve(sudoku):
     states = np.array([np.arange(1, 10, 1)])
+    # create list of objects containing all possible states
     objects = np.repeat(states, 81, 0)
-    start_values_idx = sudoku.nonzero()
+    # find indices of nonzero elements in initial sudoku
+    start_values_idx = sudoku.reshape(81).nonzero()
     objects[start_values_idx] = 0
-    objects[start_values_idx, sudoku[start_values_idx] - 1] = sudoku[start_values_idx]
-    for i in range(objects.shape[0]):
-        for j in objects[i].nonzero():
-            neighb_idx = neighbours_idx(i)
-            flag = np.logical_and(objects[neighb_idx] != 0, objects[neighb_idx] != objects[i, j]).any(axis=1)
-            if not flag.all():
-                objects[i, j] = 0
-    pass
+    objects[start_values_idx, sudoku.reshape(81)[start_values_idx] - 1] = \
+        sudoku.reshape(81)[start_values_idx]
+    is_changed = 1
+    while is_changed:
+        is_changed = 0
+        for i in range(objects.shape[0]):
+            neighb_idx = neighbours_idx(i, sudoku)
+            for j in objects[i].nonzero()[0]:
+                flag = np.logical_and(objects[neighb_idx] != 0, objects[neighb_idx] != objects[i, j]).any(axis=1)
+                if not flag.all():
+                    objects[i, j] = 0
+                    is_changed = 1
+    return objects
 
 
 if __name__ == '__main__':
