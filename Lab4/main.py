f99b6352fb587448aabecd9e32c6fa8b19c02145
anastorr <nastya.stor111@gmail.com>
2021-12-16 01:54:58 +0200

    Add code for computing resulting image

--- Lab4/main.py
+++ Lab4/main.py
@@ -1,38 +1,53 @@
 import numpy as np
-from cv2 import imread, imshow, waitKey
+from cv2 import imread, imwrite
 import time
 
 
 def norm_1(x):
-    return np.sum(x, axis=len(x.shape)-1)
+    return np.sum(abs(x), axis=-1)
 
 
-def abomination(q, g):
-    f = np.zeros((g.shape[2]+1, q.shape[0], g.shape[3]))
-    f_arg = np.zeros((g.shape[2]+1, q.shape[0], g.shape[3]), dtype='int32')
-    for i in range(2, g.shape[2]+1):
-        f[-i] = np.min(f[-i+1] + q[:, -i+1, :] + g[:, :, -i, :], axis=0)
-        f_arg[-i] = np.argmin(f[-i+1] + q[:, -i+1, :] + g[:, :, -i, :], axis=0)
-    f_arg[0, 0] = np.argmin(q[:, 0, :] + f[1], axis=0)
-    pass
+def abomination(q, g, h, w, m):
+    f = np.zeros((m, h, w))
+    f_arg = np.zeros((m, h, w), dtype='int16')
+    for i in range(2, w):
+        f[..., -i] = np.min(f[np.newaxis, ..., -i+1].repeat(m, axis=0) + q[np.newaxis, :, :, -i+1].repeat(m, axis=0) +
+                            g[:, :, :, -i+1], axis=1)
+        f_arg[..., -i] = np.argmin(f[..., -i+1] + q[:, :, -i+1] + g[:, :, :, -i+1], axis=1)
+    # solving for k*
+    ans = np.zeros((h, w), dtype='int16')
+    ans[..., 0] = np.argmin(q[:, :, 0] + f[..., 1], axis=0)
+    idx = np.arange(0, h, 1)
+    for i in range(1, w):
+        ans[..., i] = f_arg[ans[..., i-1], idx, i]
+    return ans
 
 
 def combine(imgs, masks, alpha, beta):
     m = imgs.shape[0]
+    h = imgs.shape[1]
+    w = imgs.shape[2]
     q = alpha * (1 - masks)
-    g = beta*(norm_1(imgs[:, np.newaxis, :-1, :] - imgs[np.newaxis, :, 1:, :].repeat(m, axis=0)))
-    start_time = time.time()
-    abomination(q, g)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    pass
+    g = beta*(norm_1(imgs[:, np.newaxis, :, :-1] - imgs[np.newaxis, :, :, :-1].repeat(m, axis=0))
+              + norm_1(imgs[:, np.newaxis, :, 1:] - imgs[np.newaxis, :, :, 1:].repeat(m, axis=0)))
+    ans = abomination(q, g, h, w, m)
+    return ans
 
 
 if __name__ == "__main__":
-    imgs = np.zeros((5, 1000, 1000, 3), dtype='uint8')
+    start_time = time.time()
+    h = 1000
+    w = 1000
+    m = 5
+    imgs = np.zeros((m, h, w, 3), dtype='int16')
     for i in range(1, 6):
         imgs[i-1] = imread('image_0{}.png'.format(i))
-    masks = np.zeros((5, 1000, 1000, 3), dtype='uint8')
+    masks = np.zeros((m, h, w, 3), dtype='int16')
     for i in range(1, 6):
         masks[i-1] = imread('mask_0{}.png'.format(i))
 
-    combine(imgs, masks[..., 0]/255, 1, 1)
+    k = combine(imgs, masks[..., 0]/255, 10000, 100)
+    result = imgs[k, np.arange(0, h, 1)[:, np.newaxis], np.arange(0, w, 1)[np.newaxis, :], :]
+    imwrite('result.png', result)
+    print("--- %s seconds ---" % (time.time() - start_time))
+
