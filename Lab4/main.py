0f7ff274144e6bf62784a5beff99dba5d1a272f5
anastorr <nastya.stor111@gmail.com>
2021-12-15 21:34:48 +0200

    Add computing argmins

--- Lab4/main.py
+++ Lab4/main.py
@@ -8,17 +8,18 @@ def norm_1(x):
 
 
 def abomination(q, g):
-    f = np.zeros((g.shape[2]+1, q.shape[0], 1000))
+    f = np.zeros((g.shape[2]+1, q.shape[0], g.shape[3]))
+    f_arg = np.zeros((g.shape[2]+1, q.shape[0], g.shape[3]), dtype='int32')
     for i in range(2, g.shape[2]+1):
-        f[-i] = np.min(f[-i+1] + q[:, -i+1] + g[:, :, -i+1], axis=0)
+        f[-i] = np.min(f[-i+1] + q[:, -i+1, :] + g[:, :, -i, :], axis=0)
+        f_arg[-i] = np.argmin(f[-i+1] + q[:, -i+1, :] + g[:, :, -i, :], axis=0)
+    f_arg[0, 0] = np.argmin(q[:, 0, :] + f[1], axis=0)
     pass
 
 
 def combine(imgs, masks, alpha, beta):
     m = imgs.shape[0]
-    h, w = imgs.shape[1], imgs.shape[2]
     q = alpha * (1 - masks)
-    g = np.zeros((m, m, w-1))
     g = beta*(norm_1(imgs[:, np.newaxis, :-1, :] - imgs[np.newaxis, :, 1:, :].repeat(m, axis=0)))
     start_time = time.time()
     abomination(q, g)
